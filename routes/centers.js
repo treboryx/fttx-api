9e3dfb6920e3c17926cfc745e6c3d5797e1de320
Robert <treboryx@gmail.com>
2020-08-20 03:31:44 +0300

    modified centers route, added node version in package.json, updated discord.js version

--- routes/centers.js
+++ routes/centers.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const {
-  getCabinets,
-  getCabinet,
-  createCabinet,
-  updateCabinet,
-  deleteCabinet,
-} = require("../controllers/cabinets");
+  getCenters,
+  getCenter,
+  createCenter,
+  updateCenter,
+  deleteCenter,
+} = require("../controllers/centers");
 
-const Cabinet = require("../models/Cabinet");
+const Center = require("../models/Center");
 
 const router = express.Router({ mergeParams: true });
 
 const advancedResults = require("../middleware/advancedResults");
 const { protect } = require("../middleware/auth");
 
-router
-  .route("/")
-  .get(advancedResults(Cabinet), getCabinets)
-  .post(createCabinet);
+router.route("/").get(advancedResults(Center), getCenters).post(createCenter);
 
 router
   .route("/:id")
-  .get(getCabinet)
-  .put(protect, updateCabinet)
-  .delete(protect, deleteCabinet);
+  .get(getCenter)
+  .put(protect, updateCenter)
+  .delete(protect, deleteCenter);
 
 module.exports = router;
